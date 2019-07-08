a3dd9897e464b901563d92ec014389d90d7dcea2
Chris Cantu <ccantu@homeaway.com>
2019-07-07 21:16:05 -0500

    Added @testing-library/react test to the project

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { render, wait } from '@testing-library/react'
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Check the content of the file', async () => {
+  const {getByText} = render(<App />);
+  const learnReact = 'Learn React';
+  await wait(() => getByText(learnReact));
+  expect(getByText(learnReact)).toBeTruthy();
 });
